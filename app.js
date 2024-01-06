81abd5f8666ef891bfe0f1ca039aea50053dcaa0
gallogian3 <gallogian3@hotmail.com>
2024-01-06 19:28:31 +0200

    Deploy app

--- app.js
+++ app.js
@@ -26,9 +26,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-const dbURL = 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbURL = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
 mongoose.connect(dbURL)
-   // mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
    .then(() => {
       console.log('Connection successful!');
    })
@@ -45,11 +44,13 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize({ replaceWith: '_' }));
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret';
+
 const store = MongoStore.create({
    mongoUrl: dbURL,
    touchAfter: 24 * 60 * 60,
    crypto: {
-      secret: 'thisshouldbeabettersecret'
+      secret
    }
 });
 
@@ -60,7 +61,7 @@ store.on('error', function (e) {
 const sessionConfig = {
    store,
    name: 'session',
-   secret: 'thisshouldbeabettersecret',
+   secret,
    resave: false,
    saveUninitialized: true,
    cookie: {
